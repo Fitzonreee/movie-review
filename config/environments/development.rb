17de847a2852f1fc884f576556a80ee17a5487ae
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-01-08 18:21:32 -0800

    Added scaffold and Users with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Needed for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # let Paperclip know to look there by adding that directory to its path
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
